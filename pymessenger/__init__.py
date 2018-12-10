d7c63312099cb5459be5be4a117651eac24db816
Houssam Alrifai <houssam.alrifie@gmail.com>
2018-12-10 17:12:07 +0200

    Update __init__.py

--- pymessenger/__init__.py
+++ pymessenger/__init__.py
@@ -18,9 +18,34 @@ class Element(dict):
         return json.dumps({k: v for k, v in self.iteritems() if k in self.__acceptable_keys})
 
 
+
+class QuickReply(dict):
+    __acceptable_keys = ['content_type','title','payload','image_url']
+    
+    def __init__(self,*args,**kwargs):
+        if six.PY2:
+            kwargs = {k: v for k, v in kwargs.iteritems() if k in self.__acceptable_keys}
+        else:
+            kwargs = {k: v for k, v in kwargs.items() if k in self.__acceptable_keys}
+        super(QuickReply,self).__init__(*args, **kwargs)
+    def to_json(self):
+        return json.dumps({k: v for k, v in self.iteritems() if k in self.__acceptable_keys})
+   
+
+
 class Button(dict):
     # TODO: Decide if this should do more
-    pass
+    __acceptable_keys = ["type", "title", "payload"]
+
+    def __init__(self, *args, **kwargs):
+        if six.PY2:
+            kwargs = {k: v for k, v in kwargs.iteritems() if k in self.__acceptable_keys}
+        else:
+            kwargs = {k: v for k, v in kwargs.items() if k in self.__acceptable_keys}
+        super(Button, self).__init__(*args, **kwargs)
+
+    def to_json(self):
+        return json.dumps({k: v for k, v in self.iteritems() if k in self.__acceptable_keys})
 
 # class Receipt:
 #     pass
